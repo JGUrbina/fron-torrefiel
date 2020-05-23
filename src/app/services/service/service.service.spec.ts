471d49aac049f5f3e6150637d69b2b4d5b756b8c
karem alcala <alcalakarem@gmail.com>
2020-05-23 01:55:17 -0400

    create service of services

--- src/app/services/service/service.service.spec.ts
+++ src/app/services/service/service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProductService } from './product.service';
+import { ServiceService } from './service.service';
 
-describe('ProductService', () => {
-  let service: ProductService;
+describe('ServiceService', () => {
+  let service: ServiceService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
+    service = TestBed.inject(ServiceService);
   });
 
   it('should be created', () => {
