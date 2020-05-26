b3319a5e92ff452bb9e9ba46695e857665800121
karem alcala <alcalakarem@gmail.com>
2020-05-26 11:17:56 -0400

    fix type object

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserService } from '../../services/user/user.service';
 import { User } from '../../models/user/user';
 import { Router } from '@angular/router';
@@ -71,10 +70,8 @@ export class LoginComponent implements OnInit {
     const userLogin = { userName: this.userName, password: this.password};
     this.userService.login(userLogin).subscribe(
       (data) => {
-        const {  token }  = data
-        console.log(token);
-        
-        localStorage.setItem('token', token)
+        const {  token }  = data;
+        localStorage.setItem('token', token);
         this.router.navigate(['/']);
       },
       (err) => {
