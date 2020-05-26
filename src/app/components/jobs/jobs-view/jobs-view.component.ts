66cf5e4b293ce798be1506e638e639225c8aec9f
karem alcala <alcalakarem@gmail.com>
2020-05-26 10:47:37 -0400

    jobs view

--- src/app/components/jobs/jobs-view/jobs-view.component.ts
+++ src/app/components/jobs/jobs-view/jobs-view.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobsViewComponent implements OnInit {
 
+  // views
+  public DELIVERYNOTE: string = 'delivery note';
+  public BILL: string = 'bill';
+  public CALENDAR: string = 'calendar';
+  public CHAT: string = 'chat';
+  public IMAGES: string = 'images';
+  public FIRM: string = 'firm';
+  public NOTE: string = 'note';
+
+  public sectionMenuShow: string;
+
   constructor() { }
 
   ngOnInit(): void {
