96645b7ffb21db1628fa68133959fe926b14bcd0
karem alcala <alcalakarem@gmail.com>
2020-06-10 10:06:53 -0400

    admin clients

--- src/app/components/admin/clients/clients.component.ts
+++ src/app/components/admin/clients/clients.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { DropDownOptionsService } from '../../../services/dropDownOptions/drop-down-options.service';
 import { Client } from '../../../models/client/client';
 import { ClientService } from 'src/app/services/client/client.service';
+import { Service } from '../../../models/service/service';
+import { ServiceService } from '../../../services/service/service.service';
+import { User } from '../../../models/user/user';
+import { UserService } from '../../../services/user/user.service';
 
 @Component({
   selector: 'app-clients',
@@ -10,10 +15,37 @@ import { ClientService } from 'src/app/services/client/client.service';
 export class ClientsComponent implements OnInit {
 
   public allClients: Client[];
+  public allUsers: User[];
+
+  public newService: Service;
+  public newClient: Client;
+  public optionsActivities: string[];
+  public optionsStatus: string[];
+  public optionsProvince: string[];
+  public optionsMunicipality: string[];
+  public searchNameClient: string;
+
+  public show: string = '';
+  public showView: string = '';
+  public OPTIONS: string = 'options';
+  public EDITUSER: string = 'edit user';
+  public SERVICES: string = 'service';
+  public EDITSERVICE: string = 'edit service';
 
   constructor(
-    private clientService: ClientService
+    private clientService: ClientService,
+    private serviceService: ServiceService,
+    private userService: UserService,
+    private dropDownOptions: DropDownOptionsService,
   ) {
+     // tslint:disable-next-line: max-line-length
+    this.newService = new Service(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+    // tslint:disable-next-line: max-line-length
+    this.newClient = new Client(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+    this.optionsActivities = this.dropDownOptions.getActivities();
+    this.optionsStatus = this.dropDownOptions.getStatus();
+    this.optionsProvince = this.dropDownOptions.getProvince();
+    this.optionsMunicipality = this.dropDownOptions.getMunicipality();
   }
 
   ngOnInit(): void {
@@ -29,6 +61,11 @@ export class ClientsComponent implements OnInit {
         console.log(err);
       }
     );
+  }
+
+  showOptions(view: string): void{
+    this.show = this.OPTIONS;
+    this.showView = view;
   }
 
 }
