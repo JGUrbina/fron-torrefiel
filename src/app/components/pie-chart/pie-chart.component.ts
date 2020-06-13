a88629f992ba177e6778737fa315a4d95bde28dd
karem alcala <alcalakarem@gmail.com>
2020-06-13 03:08:49 -0400

    graph

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartType } from 'chart.js';
-import { MultiDataSet, Label } from 'ng2-charts';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pie-chart',
@@ -9,15 +7,54 @@ import { MultiDataSet, Label } from 'ng2-charts';
 })
 export class PieChartComponent implements OnInit {
 
-  doughnutChartLabels: Label[] = ['BMW', 'Ford', 'Tesla'];
-  doughnutChartData: MultiDataSet = [
-    [55, 25, 20]
-  ];
-  doughnutChartType: ChartType = 'doughnut';
+  @Input() id: string;
+  @Input() lineWidth: number;
+  @Input() porcentage: number;
+  @Input() rotate: number;
+  @Input() size: number;
+
+  public searchId: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.searchId = `graph_${this.id}_${this.porcentage}`;
+    this.createGraph(this.lineWidth, this.porcentage, this.rotate, this.size);
   }
 
+  createGraph(lineWidth: number, porcentage: number, rotate: number, size: number): void {
+    console.log(this.searchId);
+    const el = document.getElementById(this.searchId); // get canvas
+
+    const options: any = {
+      percent: porcentage || 25,
+      size: size || 50,
+      lineWidth: lineWidth || 3,
+      rotate: rotate || 0
+    };
+
+    const canvas: any = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = canvas.height = options.size;
+
+    el.appendChild(canvas);
+    ctx.translate(options.size / 2, options.size / 2); // change center
+    ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+    const radius = (options.size - options.lineWidth) / 2;
+
+    const drawCircle = (color: string, lineWidthCircle: number, percent: number) => {
+      percent = Math.min(Math.max(0, percent || 1), 1);
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+      ctx.strokeStyle = color;
+      ctx.lineCap = 'round'; // butt, round or square
+      ctx.lineWidth = lineWidthCircle;
+      ctx.stroke();
+    };
+
+    drawCircle('#efefef', options.lineWidth, 100 / 100);
+    drawCircle('#cd2a00', options.lineWidth, options.percent / 100);
+  }
 }
