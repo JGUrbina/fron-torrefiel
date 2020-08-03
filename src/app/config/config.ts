fd791044f8fb3058a413be328dccb2c959bda70a
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-08-02 21:22:26 -0500

    host vercel

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
 //export const UrlApiGlobal = 'https://torrefiel.herokuapp.com';
-export const UrlApiGlobal = 'http://localhost:5001';
+export const UrlApiGlobal = 'https://back-torrefiel-git-master.ricardordzalt.vercel.app';
 
 export const SocketConfig = { url: 'https://torrefiel.herokuapp.com', options: {} };
