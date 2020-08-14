ef35694c541321d2733e2035dabfcf15982a6721
Marina <alcubilla.marina@gmail.com>
2020-08-14 02:09:59 -0500

    hora chat, resumen albaranes acomodado

--- src/app/components/jobs/chat/chat.component.ts
+++ src/app/components/jobs/chat/chat.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, Input, OnDestroy } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { ChatService } from '../../../services/chat/chat.service'
+import { getLocaleDateFormat } from '@angular/common';
+import { now } from 'jquery';
 
 
 @Component({
@@ -22,6 +24,13 @@ export class ChatComponent implements OnInit, OnDestroy {
   public allNotes: any[];
   public nickName: any[];
   public chat: any;
+  public fecha: string;
+  public CurrentYear: any;
+  public CurrentMonth: any;
+  public CurrentDay: any;
+  public CurrentHour: any;
+  public CurrentMinutes: any;
+  public CurrentSeconds: any;
 
   constructor(
     private chatService: ChatService,
@@ -32,11 +41,7 @@ export class ChatComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.loadMessages();
     this.getMessages();
-    console.log("chat", this.chat);
-    setInterval(function(){
-      this.chat = document.getElementById('main'); 
-      this.chat==null? null : this.chat.scrollTop=this.chat.scrollHeight 
-    },500)
+
   }
 
   ngOnDestroy(): void{
@@ -46,7 +51,15 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   loadMessages(){
     this.chatService.initSocket();
-    this.chatService.getHistory().subscribe(data => this.allMessages = data.reverse()), err => console.log('err', err);
+    this.chatService.getHistory().subscribe(data => {
+      console.log("chat", data)
+      this.allMessages = data.reverse();
+     
+    }), err => console.log('err', err);
+    setTimeout(function(){
+      this.chat = document.getElementById('main'); 
+      this.chat==null? null : this.chat.scrollTop=this.chat.scrollHeight 
+    },500)
   }
 
   getMessages(){
@@ -54,17 +67,32 @@ export class ChatComponent implements OnInit, OnDestroy {
   }
 
   sendMessage(){
+    this.CurrentYear = new Date().getFullYear();
+    this.CurrentMonth = new Date().getMonth() + 1;
+    this.CurrentDay = new Date().getDay();
+    this.CurrentHour = new Date().getHours();
+    this.CurrentMinutes = new Date().getMinutes();
+    this.CurrentSeconds = new Date().getSeconds();
+
+    this.fecha = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}T${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}.`
     this.nickName = localStorage.getItem('some-key')==null ? 'nickName' :  JSON.parse(localStorage.getItem('some-key')).name
     const msg = {
       text: this.newMessage,
+      createdAt: this.fecha,
       user: { userName: this.nickName , _id: 12331412 }
     };
     this.chatService.sendMessage(msg);
     this.allMessages.push(msg);
     this.newMessage = '';
+    console.log("NW", this.newMessage)
+    setTimeout(function(){
+      this.chat = document.getElementById('main'); 
+      this.chat==null? null : this.chat.scrollTop=this.chat.scrollHeight 
+    },500)
   }
 
   emitEvent(): void{
     this.closeWindow.emit('');
   }
+
 }
