69989f747a97c738c7d08332f74e4029733f04ca
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-08-27 14:11:27 -0500

    url correcta del servidor

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,15 +1,15 @@
 
 //export const UrlApiGlobal = 'https://back-torrefiel-git-master.ricardordzalt.vercel.app';
 
-export const UrlApiGlobal = 'http://localhost:5001';
-export const SocketConfig = { url: 'http://localhost:5001', options: {} };
+// export const UrlApiGlobal = 'http://localhost:5001';
+// export const SocketConfig = { url: 'http://localhost:5001', options: {} };
 
 // export const UrlApiGlobal = 'https://torrefiel-back.herokuapp.com';
 // export const SocketConfig = { url: 'https://torrefiel-back.herokuapp.com', options: {} };
 
-// export const UrlApiGlobal = 'http://68.183.32.234';
-// export const SocketConfig = { url: 'http://68.183.32.234', options: {} };
+export const UrlApiGlobal = 'http://68.183.32.234';
+export const SocketConfig = { url: 'http://68.183.32.234', options: {} };
 
-// app torefiel
+//app torefiel
 // export const UrlApiGlobal = 'http://apptorrefiel.com';
 // export const SocketConfig = { url: 'http://apptorrefiel.com', options: {} };
