20b29c9ec2c17a410b354398de8bfcc9658a89a8
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-08-28 15:39:21 -0500

    notif listas parece 2.o

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,18 +1,8 @@
-import { UserService } from 'src/app/services/user/user.service';
-import { Component, OnInit, ViewChild, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component,  Input, Output, EventEmitter } from '@angular/core';
 import { FullCalendarComponent } from '@fullcalendar/angular';
-import { EventInput } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGrigPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import esLocale from '@fullcalendar/core/locales/es';
-import { User } from 'src/app/models/user/user';
-import { Service } from 'src/app/models/service/service';
-import { ServiceService } from 'src/app/services/service/service.service';
 import * as moment from 'moment';
 import 'moment/locale/es'  // without this line it didn't work
 moment.locale('es')
-
 
 @Component({
   selector: 'app-notifications',
@@ -20,49 +10,24 @@ moment.locale('es')
   styleUrls: ['./notifications.component.scss']
 })
 
-export class NotificationsComponent implements OnInit, OnDestroy { 
+export class NotificationsComponent { 
   
-
   @Output() deleteNotificationIndex: EventEmitter<Number> = new EventEmitter();
-  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
-  @Input() events: any[];
   @Input() allNotifications: any[];
 
-  public calendarLocale: any;
   public calendarVisible: boolean; 
-  public calendarPlugins: any[];
-  public calendarWeekends: boolean;
-  public calendarEvents: EventInput[];
-
-  public showEvent: boolean = false;
-  public selectedEvent: Service;
-  public allWorkers: [User];
   public numberNotifications: any;
-
 
   constructor(
   ) {
     this.calendarVisible = true;
-    this.calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
-    this.calendarWeekends = true;
-    this.calendarEvents = [];
-    this.calendarLocale = esLocale;
     this.allNotifications = [];
     this.numberNotifications = '';
-  }
-
-  ngOnInit(): void {
-    console.log('oninit notif');
-  }
-
-  ngOnDestroy(): void {
-    console.log('ondestroy notif');
   }
 
   setFecha(createdAt){
     return moment(new Date(createdAt)).fromNow()
   }
-
 
   deleteNotification(id){
     this.deleteNotificationIndex.emit(id);
