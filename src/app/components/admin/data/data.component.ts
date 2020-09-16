7a700cf1951b62f8e9c2b56e670bc96a64595460
Jose Urbina <j.urbina.0183@Gmail.com>
2020-09-15 23:55:12 -0300

    30% data

--- src/app/components/admin/data/data.component.ts
+++ src/app/components/admin/data/data.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
+import { ServiceService } from 'src/app/services/service/service.service'
+import { ClientService } from 'src/app/services/client/client.service';
+
 
 @Component({
   selector: 'app-data',
@@ -9,14 +13,82 @@ import { EventEmitter } from '@angular/core';
 export class DataComponent implements OnInit {
 
   @Output () closeWindow = new EventEmitter();
+public array: any
+public nickName: any
+public numWorks: any
+public masRepetida: string
+public cont: number
+public arr: any
+public clients: any
+public allservice: any
 
-  constructor() { }
+  constructor(
+    private services : ServiceService,
+    private userService : UserService,
+    private clientService : ClientService
+  ) { 
+    this.array= []
+    this.arr = []
+    this.cont = 0
+  }
 
-  ngOnInit(): void {
+ngOnInit() {
+  this.allService() 
+   this.userWorks()
+   this.allClient()
   }
 
   emitEvent(): void{
     this.closeWindow.emit('');
   }
+  allService() {
+   
+    this.services.getServices().subscribe(
+      data => {
+        this.allservice = data.services
+        let all = 0
+        data.services.forEach(service => {
+          
+          this.array += (service.status + '-')
+          
+        })
+        all =  this.array.split('-').length - 1
+       this.masRepetida = this.array.split('-').sort((a,b) => this.array.split('-').filter(v => v===a).length
+        - this.array.split('-').filter(v => v===b).length).pop()
+        let cont = this.array.split('-').filter(s => s=== this.masRepetida).length
+        this.cont = (cont * 100 / all)
+        this.cont.toFixed(3)
+        console.log(this.masRepetida, this.cont)
+      },
+       err => { console.log(err)}
+    )
+  }
+  
+ 
+  userWorks(){
+    this.userService.getUsers().subscribe(
+      data => {
+        let user = data.sort(((a, b) => a.works.length - b.works.length))[data.length-1]
+       this.nickName = user.userName
+       this.numWorks =  user.works.length
+      }, err => {
+        console.log(err)
+      }
+    )
+  }
+
+allClient() {
+  this.clientService.getClients().subscribe(
+    data => {
+    this.clients =  data.sort(((a, b) => a.services.length - b.services.length)).slice(-4).reverse()
+    this.clients.forEach(client => console.log(client.services.length, this.allservice.length ))
+      // console.log(this.clients)
+    },
+    err => {
+      console.log(err)
+    }
+  )
+}
+
 
 }
