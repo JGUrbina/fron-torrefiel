9b92fa624fa0ea1a1922b7193d68b1a6da599828
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-10-04 11:26:08 -0500

    calendario modal

--- src/app/components/card-calendar/card-calendar.component.ts
+++ src/app/components/card-calendar/card-calendar.component.ts
@@ -21,6 +21,8 @@ import {
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 
+import { Service } from '../../models/service/service';
+
 registerLocaleData(localeEs);
 
 @Component({
@@ -34,6 +36,8 @@ export class CardCalendarComponent implements OnInit {
   @Input() eventsInput: any[];
 
   public allWorkers: [User];
+
+  public selectedWork: any;
 
   view: CalendarView = CalendarView.Day;
 
@@ -72,15 +76,17 @@ export class CardCalendarComponent implements OnInit {
   setEvents() {
     this.allWorkers.forEach(worker => {
       worker.works.forEach(work => {
-        console.log('worker', worker);
         const tmpService = this.eventsInput.filter(event => event._id === work)[0];
-        const tmpHours = Number(tmpService.startHours.split(':')[0]);
-        const tmpEvent = {
-          start: addHours(new Date(tmpService.startDate.replace('.000Z', '')), tmpHours),
-          title: `${tmpService.startHours} ${worker.name}`,
-          color: { primary: '#cd2a00;', secondary: worker.color }
-        }
-        this.events.push(tmpEvent);
+        if (!!tmpService) {
+          const tmpHours = Number(tmpService.startHours.split(':')[0]);
+          const tmpEvent = {
+            start: addHours(new Date(tmpService.startDate.replace('.000Z', '')), tmpHours),
+            title: `${tmpService.startHours} ${worker.name}`,
+            color: { primary: '#cd2a00;', secondary: worker.color },
+            id: tmpService._id
+          }
+          this.events.push(tmpEvent);
+        };
       });
     });
     this.refresh.next()
@@ -89,110 +95,7 @@ export class CardCalendarComponent implements OnInit {
   handleEvent(action: string, event: CalendarEvent): void {
     this.modalData = { event, action };
     this.modal.open(this.modalContent, { size: 'lg' });
+    this.selectedWork = this.eventsInput.find(event => event._id === this.modalData.event.id);
+    console.log('selected', this.selectedWork);
   }
 }
-
-
-
-
-
-
-
-
-
-//import { UserService } from 'src/app/services/user/user.service';
-// import { Component, OnInit, ViewChild, Input } from '@angular/core';
-// import { FullCalendarComponent } from '@fullcalendar/angular';
-// import { EventInput } from '@fullcalendar/core';
-// import dayGridPlugin from '@fullcalendar/daygrid';
-// import timeGrigPlugin from '@fullcalendar/timegrid';
-// import interactionPlugin from '@fullcalendar/interaction';
-// import esLocale from '@fullcalendar/core/locales/es';
-// import { User } from 'src/app/models/user/user';
-// import { Service } from 'src/app/models/service/service';
-
-// @Component({
-//   selector: 'app-card-calendar',
-//   templateUrl: './card-calendar.component.html',
-//   styleUrls: ['./card-calendar.component.scss']
-// })
-// export class CardCalendarComponent implements OnInit {
-
-//   @ViewChild('calendar') calendarComponent: FullCalendarComponent;
-//   @Input() events: any[];
-
-//   public calendarLocale: any;
-//   public calendarVisible: boolean;
-//   public calendarPlugins: any[];
-//   public calendarWeekends: boolean;
-//   public calendarEvents: EventInput[];
-//   public allDaySlot: boolean;
-
-//   public showEvent: boolean = false;
-//   public selectedEvent: Service;
-//   public allWorkers: [User];
-
-//   public customEvents: any;
-
-
-//   constructor(
-//     private userService: UserService
-//   ) {
-//     this.calendarVisible = true;
-//     this.calendarPlugins = [timeGrigPlugin, interactionPlugin, dayGridPlugin];
-//     this.calendarWeekends = true;
-//     this.calendarEvents = [];
-//     this.calendarLocale = esLocale;
-//     this.allDaySlot = false;
-//   }
-
-
-
-//   ngOnInit(): void {
-//     this.getAllWorkers();
-//   }
-
-//   changeShowEvent(event){
-//     this.showEvent = !this.showEvent;
-//     const id = event.event.id;
-//     const eventFiltered = this.events.filter(event => event._id === id);
-//     this.selectedEvent = eventFiltered[0];
-//   }
-
-//   toggleVisible() {
-//     this.calendarVisible = !this.calendarVisible;
-//   }
-
-//   getAllWorkers() {
-//     this.userService.getUsers().subscribe(
-//       data => {
-//         this.allWorkers = data;
-//         this.showEvents();
-//       }, err => console.log('err', err));
-//   }
-
-//   showEvents() {
-
-//     this.allWorkers.forEach(worker => {
-//       this.events.forEach(event => {
-//         if(worker.works.includes(event._id)){
-//           const start = event.startDate.split('T')[0];
-//           console.log("Start",start)
-//           let { _id: id, name: title, startHours } = event;
-//           title = startHours;
-//           title += ` ${worker.name}`;
-//           const color = worker.color;
-//           const calEvent = [{ id, start, title, color, startHours }];
-//           //this.calendarEvents.push(calEvent); //no funciona así, ni con push de object :(
-//           this.calendarEvents = this.calendarEvents.concat(calEvent);
-//         };
-//       });
-//     });
-//     this.calendarEvents.sort((a, b) => a.startHours.localeCompare(b.startHours));
-//     console.log('eventos1', this.calendarEvents);
-//     this.customEvents = { events: this.calendarEvents, color: 'yellow', textColor: 'black' }
-//   }
-
-
-
-// }
